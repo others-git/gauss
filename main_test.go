a369a0d92ccca7ff9594a6bf8d122484d2425bc6
Brian G. Shacklett <brian@digital-traffic.net>
2017-09-11 01:55:55 -0500

    WIP: working on writing integration test for diff()

---
+++ main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/beard1ess/gauss/parsing"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"reflect"
+	"testing"
+)
+
+func TestDiff(t *testing.T) {
+
+	var expected, actual parsing.ConsumableDifference
+
+	assert := assert.New(t)
+	require := require.New(t)
+
+	testBuffer := bytes.NewBuffer(nil) // A testing writer
+
+	diff(
+		"./tests/one.json",
+		"./tests/two.json",
+		"machine",
+		testBuffer,
+	)
+
+	result, err := ioutil.ReadAll(testBuffer)
+	require.Nil(err, "The test buffer should be readable")
+
+	testData, err := ioutil.ReadFile("./tests/diff.json")
+	require.Nil(err, "The test diff should be readable.")
+
+	json.Unmarshal(testData, &expected)
+	require.Nil(err, "The test data should be unmarshaled without error.")
+
+	temp, _ := json.Marshal(expected)
+	fmt.Println(temp)
+
+	json.Unmarshal(result, &actual)
+	assert.Nil(err, "The result should be unmarshaled without error.")
+
+	assert.Equal(
+		reflect.DeepEqual(expected, actual),
+		true,
+		"The diff of one.json and two.json should equal the test diff.",
+	)
+}
