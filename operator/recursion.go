9b76a3a025e016781576036953eeacd2e9650f45
beard1ess <mittg.aaron@gmail.com>
2017-09-12 04:00:49 -0500

    fixing

--- operator/recursion.go
+++ operator/recursion.go
@@ -7,133 +7,143 @@ import (
 	"fmt"
 )
 
-var ObjectDiff = parsing.ConsumableDifference{}
+func recursion(
 
-func keys(original parsing.Keyvalue, modified parsing.Keyvalue, path []string) {
+	original parsing.Keyvalue,
+	modified parsing.Keyvalue,
+	input_path []string,
+	ObjectDiff parsing.ConsumableDifference,
 
-	for k, v := range modified {
-		if parsing.IndexOf(parsing.ListStripper(original), k) == -1 {
-			added := parsing.AddedDifference{Path: parsing.PathFormatter(path), Key: k, Value: v}
-			ObjectDiff.Added = append(ObjectDiff.Added, added)
-			delete(modified, k)
-			fmt.Println("DELETED:  ",k)
-		}
-	}
-	for k, v := range original {
-		if parsing.IndexOf(parsing.ListStripper(modified), k) == -1 {
-			removed := parsing.RemovedDifference{Path: parsing.PathFormatter(path), Key: k, Value: v}
-			ObjectDiff.Removed = append(ObjectDiff.Removed, removed)
-			delete(original, k)
-			fmt.Println("DELETED:  ",k)
-		}
-	}
-
-	recursion(original, modified, path)
-}
-
-func recursion(original parsing.Keyvalue, modified parsing.Keyvalue, input_path []string) {
+	) parsing.ConsumableDifference {
 
 	path := make([]string, len(input_path))
 	copy(path, input_path)
 	if reflect.DeepEqual(original, modified) {
-		return
+		return ObjectDiff
 	}
 
 	if !(parsing.UnorderedKeyMatch(original, modified)) {
 
-		keys(original, modified, path)
-		return
+		for k, v := range modified {
+			if parsing.IndexOf(parsing.ListStripper(original), k) == -1 {
+				added := parsing.AddedDifference{Path: parsing.PathFormatter(path), Key: k, Value: v}
+				ObjectDiff.Added = append(ObjectDiff.Added, added)
+				delete(modified, k)
+			}
+		}
+		for k, v := range original {
+			if parsing.IndexOf(parsing.ListStripper(modified), k) == -1 {
+				removed := parsing.RemovedDifference{Path: parsing.PathFormatter(path), Key: k, Value: v}
+				ObjectDiff.Removed = append(ObjectDiff.Removed, removed)
+				delete(original, k)
+			}
+		}
 
+		ObjectDiff = recursion(original, modified, path, ObjectDiff)
+		return ObjectDiff
+
+	} else if len(parsing.ListStripper(original)) > 1 || len(parsing.ListStripper(modified)) > 1 {
+		for k := range original {
+			ObjectDiff = recursion(
+				parsing.Keyvalue{k: original[k]},
+				parsing.Keyvalue{k: modified[k]},
+				path, ObjectDiff)
+			return ObjectDiff
+		}
 	}
 	for k := range original {
-			var valOrig, valMod interface{}
-			if reflect.TypeOf(original).Kind() == reflect.String {
-				valOrig = original
-			} else {
-				valOrig = original[k]
-			}
-			if reflect.TypeOf(modified).Kind() == reflect.String {
-				valMod = modified
-			} else {
-				valMod = modified[k]
-			}
 
-			if !(reflect.DeepEqual(valMod, valOrig)) {
-				if reflect.TypeOf(valOrig).Kind() == reflect.Map {
-					path = append(path, k)
-					recursion(parsing.Remarshal(valOrig), parsing.Remarshal(valMod), path)
-					return
-				} else if reflect.TypeOf(valOrig).Kind() == reflect.Slice {
-					var match bool
-					valOrig, _ := valOrig.([]interface{})
-					valMod, _ := valMod.([]interface{})
-					path = append(path, k)
-					npath := make([]string, len(path))
-					if len(valOrig) != len(valMod) {
-						if len(valOrig) > len(valMod) {
-							for i := range valOrig {
-								for ii := range valMod {
-									if reflect.DeepEqual(valOrig[i], valMod[ii]) {
-										match = true
-									} else if i == ii {
-										iter := len(path) - 1
-										path[iter] = path[iter] + "[" + strconv.Itoa(i) + "]"
-										recursion(parsing.Remarshal(valOrig[i]), parsing.Remarshal(valMod[i]), path)
-									}
-								}
-								if !(match) {
-									removed := parsing.RemovedDifference{Path: parsing.PathFormatter(path),
-										Key:                                   k, Value: valOrig}
-									ObjectDiff.Removed = append(ObjectDiff.Removed, removed)
-								} else {
-									match = false
+		var valOrig, valMod interface{}
+		if reflect.TypeOf(original).Kind() == reflect.String {
+			valOrig = original
+		} else {
+			valOrig = original[k]
+		}
+		if reflect.TypeOf(modified).Kind() == reflect.String {
+			valMod = modified
+		} else {
+			valMod = modified[k]
+		}
+
+		if !(reflect.DeepEqual(valMod, valOrig)) {
+			if reflect.TypeOf(valOrig).Kind() == reflect.Map {
+				path = append(path, k)
+				ObjectDiff = recursion(parsing.Remarshal(valOrig), parsing.Remarshal(valMod), path, ObjectDiff)
+			} else if reflect.TypeOf(valOrig).Kind() == reflect.Slice {
+				var match bool
+				valOrig, _ := valOrig.([]interface{})
+				valMod, _ := valMod.([]interface{})
+				path = append(path, k)
+				npath := make([]string, len(path))
+				if len(valOrig) != len(valMod) {
+					if len(valOrig) > len(valMod) {
+						for i := range valOrig {
+							for ii := range valMod {
+								if reflect.DeepEqual(valOrig[i], valMod[ii]) {
+									match = true
+								} else if i == ii {
+									iter := len(path) - 1
+									path[iter] = path[iter] + "[" + strconv.Itoa(i) + "]"
+									ObjectDiff = recursion(parsing.Remarshal(valOrig[i]), parsing.Remarshal(valMod[i]),
+										path, ObjectDiff)
+									return ObjectDiff
 								}
 							}
-
-						} else {
-							for i := range valMod {
-								for ii := range valOrig {
-									if reflect.DeepEqual(valOrig[ii], valMod[i]) {
-										match = true
-									} else if i == ii {
-										iter := len(path) - 1
-										path[iter] = path[iter] + "[" + strconv.Itoa(i) + "]"
-										recursion(parsing.Remarshal(valOrig[i]), parsing.Remarshal(valMod[i]), path)
-									}
-								}
-								if !(match) {
-									added := parsing.AddedDifference{Path: parsing.PathFormatter(path),
-										Key:                               k, Value: valMod}
-									ObjectDiff.Added = append(ObjectDiff.Added, added)
-								} else {
-									match = false
-								}
+							if !(match) {
+								fmt.Println("asdf")
+								removed := parsing.RemovedDifference{Path: parsing.PathFormatter(path),
+									Key: k, Value: valOrig}
+								ObjectDiff.Removed = append(ObjectDiff.Removed, removed)
+							} else {
+								match = false
 							}
 						}
 					} else {
-						for i := range valOrig {
-							copy(npath, path)
-							if !(reflect.DeepEqual(valOrig[i], valMod[i])) {
-								iter := len(npath) - 1
-								npath[iter] = npath[iter] + "[" + strconv.Itoa(i) + "]"
-								recursion(parsing.Remarshal(valOrig[i]), parsing.Remarshal(valMod[i]), npath)
+						for i := range valMod {
+							for ii := range valOrig {
+								if reflect.DeepEqual(valOrig[ii], valMod[i]) {
+									match = true
+								} else if i == ii {
+									iter := len(path) - 1
+									path[iter] = path[iter] + "[" + strconv.Itoa(i) + "]"
+									ObjectDiff = recursion(parsing.Remarshal(valOrig[i]), parsing.Remarshal(valMod[i]),
+										path, ObjectDiff)
+								}
+							}
+							if !(match) {
+								added := parsing.AddedDifference{Path: parsing.PathFormatter(path),
+									Key:                               k, Value: valMod}
+								ObjectDiff.Added = append(ObjectDiff.Added, added)
+							} else {
+								match = false
 							}
 						}
-						return
 					}
 				} else {
-					changed := parsing.ChangedDifference{Path: parsing.PathFormatter(path), Key: k,
-						OldValue:                              valOrig, NewValue: valMod}
-					ObjectDiff.Changed = append(ObjectDiff.Changed, changed)
-					return
+					for i := range valOrig {
+						copy(npath, path)
+						if !(reflect.DeepEqual(valOrig[i], valMod[i])) {
+							iter := len(npath) - 1
+							npath[iter] = npath[iter] + "[" + strconv.Itoa(i) + "]"
+							ObjectDiff = recursion(parsing.Remarshal(valOrig[i]), parsing.Remarshal(valMod[i]),
+								npath, ObjectDiff)
+							return ObjectDiff
+						}
+					}
 				}
+			} else {
+				changed := parsing.ChangedDifference{Path: parsing.PathFormatter(path),
+				Key: k, OldValue: valOrig, NewValue: valMod}
+				ObjectDiff.Changed = append(ObjectDiff.Changed, changed)
+				return ObjectDiff
 			}
 		}
-		return
 	}
+	return ObjectDiff
+}
 
 
 func Recursion(original parsing.Keyvalue, modified parsing.Keyvalue, path []string) parsing.ConsumableDifference {
-	recursion(original, modified, path)
-	return ObjectDiff
+	var ObjectDiff = parsing.ConsumableDifference{}
+	return recursion(original, modified, path, ObjectDiff)
 }
