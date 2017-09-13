837e2f23513af3c4ad528830869fa1d59b978450
aaro8584 <aaron.mittag@rackspace.com>
2017-09-12 22:54:58 -0500

    working on reader

---
+++ parsing/consume.go
@@ -0,0 +1,44 @@
+package parsing
+
+import (
+	"encoding/json"
+	"github.com/go-yaml/yaml"
+
+	"reflect"
+)
+
+
+
+type readerType struct {}
+
+func (r *readerType) yamlMarshal() interface{} {
+	var placeholder interface{}
+
+	return placeholder
+}
+func (r *readerType) yamlUnmarshal() interface{} {
+	var placeholder interface{}
+
+	return placeholder
+}
+
+func (r *readerType) jsonUnmarshal() interface{} {
+	var placeholder interface{}
+
+	return placeholder
+}
+func (r *readerType) jsonMarshal() interface{} {
+	var placeholder interface{}
+
+	return placeholder
+}
+
+
+func Detector() readerType{
+	var reader readerType
+
+	return reader
+}
+
+
+
